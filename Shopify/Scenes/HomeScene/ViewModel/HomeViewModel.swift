def48c4009ae9dfbdf705543eda629fb1f119dbf
ahmed shehta <shehta029@gmail.com>
2022-07-04 13:51:35 -0700

    adding network layer

--- Shopify/Scenes/HomeScene/ViewModel/HomeViewModel.swift
+++ Shopify/Scenes/HomeScene/ViewModel/HomeViewModel.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+class HomeViewModel {
+    var usersArray: [SmartCollection]? {
+        didSet {
+            bindingData(usersArray,nil)
+        }
+    }
+    var error: Error? {
+        didSet {
+            bindingData(nil, error)
+        }
+    }
+    let apiService : ApiService  // create api service object
+    var bindingData: (([SmartCollection]?,Error?) -> Void) = {_, _ in }
+    init(apiService: ApiService = NetworkManger()) {
+        self.apiService = apiService
+    }
+    
+    
+    
+    func fetchData() {
+        apiService.fetchbranchs(endPoint: "smart_collections.json") { branchs, error in
+            if let branchs = branchs {
+                self.usersArray = branchs
+            }
+            
+            if let error = error {
+                self.error = error
+            }
+            
+        }
+        
+        
+        
+        
+    
+    }
+    
+    
+    
+}
