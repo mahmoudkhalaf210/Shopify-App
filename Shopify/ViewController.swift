e981787d33800af4864736e0102d94b0da7bdfaf
mahmoud khalifa <mahmoudkhalifa187@gmail.com>
2022-07-04 11:03:29 -0700

    add print

--- Shopify/ViewController.swift
+++ Shopify/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello world")
+        print("ahmed")
         // Do any additional setup after loading the view.
     }
 
