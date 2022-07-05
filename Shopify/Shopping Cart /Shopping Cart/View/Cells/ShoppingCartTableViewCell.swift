a45b1da34b5f432f55403fba564f8f9e636d4a6c
mahmoud khalifa <mahmoudkhalifa187@gmail.com>
2022-07-04 23:54:35 -0700

    add Shopping Cart structure

---
+++ Shopify/Shopping Cart /Shopping Cart/View/Cells/ShoppingCartTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ShoppingCartTableViewCell.swift
+//  Shopify
+//
+//  Created by Macintosh on 04/07/2022.
+//
+
+import UIKit
+
+class ShoppingCartTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
