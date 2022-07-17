c2ef8007ec47ec44390e8fc8608f29a27ed682d2
mahmoud khalifa <mahmoudkhalifa187@gmail.com>
2022-07-17 15:51:19 -0700

    add Encoding

---
+++ Shopify/Extentions/Encoding+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  Encoding+Extention.swift
+//  Shopify
+//
+//  Created by Mostafa Elbadawy on 13/07/2022.
+//
+
+import Foundation
+
+extension Encodable {
+    func asDictionary() throws -> [String: Any] {
+        let data = try JSONEncoder().encode(self)
+        guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] else {
+            throw NSError()
+        }
+        return dictionary
+    }
+}
